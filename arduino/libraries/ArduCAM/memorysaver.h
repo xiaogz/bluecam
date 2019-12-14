0d405468e553ec342c3fba15db26cca4754c5184
GuangZhi Xiao <GuangZhi.Xiao@outlook.com>
2019-12-14 13:43:03 -0500

    Add ArduCAM sample code
    TODO: have to cleanup uneeded code

---
+++ arduino/libraries/ArduCAM/memorysaver.h
@@ -0,0 +1,19 @@
+#ifndef _MEMORYSAVER_
+#define _MEMORYSAVER_
+
+//Only when using raspberry,enable it
+//#define RASPBERRY_PI
+
+//There are two steps you need to modify in this file before normal compilation
+//Only ArduCAM Shield series platform need to select camera module, ArduCAM-Mini series platform doesn't
+
+//Step 1: select the hardware platform, only one at a time
+//#define OV2640_MINI_2MP
+//#define OV3640_MINI_3MP
+//#define OV5642_MINI_5MP
+//#define OV5642_MINI_5MP_BIT_ROTATION_FIXED
+#define OV2640_MINI_2MP_PLUS
+//#define OV5642_MINI_5MP_PLUS
+//#define OV5640_MINI_5MP_PLUS
+
+#endif	//_MEMORYSAVER_
